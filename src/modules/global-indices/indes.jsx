3972d1c11ab6319f570d7c7789a89c42cdededa3
ankitkumar ravidas <ankitkumar.ravidas@nw18.com>
2025-01-12 18:29:44 +0530

    temp changes

---
+++ src/modules/global-indices/indes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GlobalIndicesContainer = () => {
+  return <div>GlobalIndicesContainer</div>;
+};
+
+export default GlobalIndicesContainer;
---
+++ src/pages/global-indices/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GlobalIndicesContainer from "../../modules/global-indices/indes";
+
+const GlobalIndices = () => {
+  return <GlobalIndicesContainer />;
+};
+
+export default GlobalIndices;
